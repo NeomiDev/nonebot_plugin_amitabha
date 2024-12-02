c90fdc706b550f0a83456f57c25e943018c8faed
kaguya <1435608435@qq.com>
2024-12-02 22:38:13 +0800

    🐛fix: known issues

--- README.md
+++ README.md
@@ -5,17 +5,78 @@
   <a href=""><img src="http://touxiang.ikkss.com/avatar/201907/2019070715343589.jpg" width="200" height="200" alt="阿弥陀佛"></a>
 </p>
 
-## nonebot_plugin_Amitabha
+## nonebot_plugin_amitabha
 
-基于nonebot2的高效半自动群聊念佛插件
-
-## 南无阿弥陀佛！
+基于nonebot2的群聊自动念佛插件
 
 </div>
+
+</details>
+
+## 📖 介绍
+
+### 南无阿弥陀佛！
 
 众所周知，以任何形式如诵读、传播、印刷佛经者都将累积对应功德。现今科学技术越发先进，自然不必拘泥于念佛的形式如何。为了打造高效、便捷、一体化的“云念佛”体系，我写了这个插件。
 
 欢迎广大开发者和感兴趣的朋友pr、使用。愿佛祖保佑你们，功德无量，阿弥陀佛！
+
+## 💿 安装
+
+<details open>
+<summary>使用 nb-cli 安装</summary>
+在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装
+
+    nb plugin install nonebot_plugin_amitabha
+
+</details>
+
+<details>
+<summary>使用包管理器安装</summary>
+在 nonebot2 项目的插件目录下, 打开命令行, 根据你使用的包管理器, 输入相应的安装命令
+
+<details>
+<summary>pip</summary>
+
+    pip install nonebot_plugin_amitabha
+</details>
+<details>
+<summary>pdm</summary>
+
+    pdm add nonebot_plugin_amitabha
+</details>
+<details>
+<summary>poetry</summary>
+
+    poetry add nonebot_plugin_amitabha
+</details>
+<details>
+<summary>conda</summary>
+
+    conda install nonebot_plugin_amitabha
+</details>
+
+打开 nonebot2 项目根目录下的 `pyproject.toml` 文件, 在 `[tool.nonebot]` 部分追加写入
+
+    plugins = ["nonebot_plugin_amitabha"]
+
+</details>
+
+## ⚙️ 配置
+
+在 nonebot2 项目的`.env`文件中添加下表中的必填配置
+
+| 配置项 | 必填 | 默认值 | 说明 |
+|:-----:|:----:|:----:|:----:|
+| 配置项1 | 是 | 无 | 配置说明 |
+| 配置项2 | 否 | 无 | 配置说明 |
+
+## 🎉 使用
+### 指令表
+| 指令 | 权限 | 需要@ | 范围 | 说明 |
+|:-----:|:----:|:----:|:----:|:----:|
+| 开启念佛模式 | 群员 | 否 | 群聊 | 进入念佛虚拟环境 |
+| 关闭念佛模式 | 群员 | 否 | 群聊 | 推出念佛虚拟环境 |
 
 ---
 
@@ -30,8 +91,7 @@
 
 * [ ]  开始念佛/诵经 [佛经名]
 * [ ]  开始持咒 [佛咒名]
-* [ ]  暂停念佛/诵经/持咒
+* [ ]  停止念佛/诵经/持咒
 * [ ]  每日禅修
 * [ ]  语音诵经 [佛经音频名]
-* [ ]  佛光普照模式（若检测到群内晦气信息则自动选择合适的佛经念诵）
 * [ ]  （待追加...）
--- nonebot-plugin-amitabha/__init__.py
+++ nonebot-plugin-amitabha/__init__.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 import re
-from typing import NoReturn
+from typing import Tuple
 
 import httpx
 from nonebot import on_command, logger
@@ -11,28 +12,28 @@ On = on_command("fo-on", aliases={"开启念佛模式", "开启诵经模式"})
 Off = on_command("fo-off", aliases={"关闭念佛模式", "关闭诵经模式"})
 
 
-async def create_cache(group_id, group_name, group_card) -> NoReturn:
+async def create_cache(group_id, group_name, bot_card) -> None:
     """创建群信息缓存"""
     # 若不存在文件夹则进行创建
-    if not os.path.exists("GroupCache"):
-        logger.warning("对应文件夹不存在，将创建新文件夹")
-        os.mkdir("GroupCache")
+    if not Path("GroupCache").exists():
+        Path("GroupCache").mkdir()
     #  腾讯qq群头像原生接口
-    group_profile = 'http://p.qlogo.cn/gh/{}/{}/640/'.format(group_id,group_id)
-    img = httpx.get(group_profile).content
-    with open('GroupCache/{}：{}.jpg'.format(group_name, group_card), 'wb') as f:
-        f.write(img)
-        f.close()
-        logger.success("缓存文件创建成功~")
+    group_profile = f'http://p.qlogo.cn/gh/{group_id}/{group_id}/640/'
+    async with httpx.AsyncClient() as client:
+        img = await client.get(group_profile)
+        save_path = f'GroupCache/{group_id}_{group_name}_{bot_card}.jpg'
+        with open(save_path, 'wb') as f:
+            f.write(img.content)
 
 
-async def load_cache() -> tuple[str, str, str]:
-    """读取群信息缓存"""
-    for filename in os.listdir("GroupCache/"):
-        group_name = re.findall('(.*?)：.*?\.jpg', filename)[0]
-        bot_card = re.findall('.*?：(.*?)\.jpg', filename)[0]
-        logger.success("读取群信息缓存成功！")
-        return group_name, bot_card, filename
+async def load_cache(group_id: int)-> Tuple[str]:
+    """按群组id读取群信息缓存"""
+    files = os.listdir("GroupCache/")
+    for f in files:
+        if str(group_id) in f:
+            matcher = re.findall('(\d*)_(.*)_(.*)\.jpg', f)[0]
+            logger.success("读取群信息缓存成功！")
+            return matcher[0], matcher[1], matcher[2], f
 
 
 @On.handle()
@@ -59,7 +60,7 @@ async def fo_on(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
         user_id=int(user_id),
         card="功德无量"
     )
-    profile = "http://m.gx8899.com/uploads/allimg/2016070112/1irc0riem02.jpg"
+    profile = "https://gitee.com/Kaguyaaa/MyImageBed/raw/master/amitabha.jpg"
     await bot.call_api(
         "set_group_portrait",
         group_id=group_id,
@@ -72,8 +73,7 @@ async def fo_on(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
 
 @Off.handle()
 async def fo_off(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
-    group_name, group_card, filename = await load_cache()
-    group_id = event.group_id
+    group_id, group_name, bot_card, filename = await load_cache(event.group_id)
     user_id = bot.self_id
     await bot.set_group_name(
         group_id=group_id,
@@ -81,14 +81,14 @@ async def fo_off(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
     await bot.set_group_card(
         group_id=group_id,
         user_id=int(user_id),
-        card=group_card
+        card=bot_card
     )
     abs_filepath = os.path.abspath('GroupCache/' + filename)
     await bot.call_api(
         "set_group_portrait",
-        group_id=group_id,
+        group_id=event.group_id,
         file="file:///" + abs_filepath  # 格式化绝对路径
     )
-    await bot.set_group_whole_ban(group_id=group_id, enable=False)  # 关闭清净模式
+    await bot.set_group_whole_ban(group_id=event.group_id, enable=False)  # 关闭清净模式
     logger.success("念佛模式已关闭")
     await matcher.finish("阿弥陀佛！您已关闭念佛模式")
